9518b56a822d87e11998a8962e139af9705dd152
Pedro Pereira <pedro.2108@hotmail.com>
2022-05-23 23:04:19 -0300

    style: Table component styled

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { deleteExpense, getExpenseToUpdate } from '../actions';
+import editIcon from '../assets/edit-icon.svg';
+import deleteIcon from '../assets/delete-icon.svg';
 
 class Table extends Component {
   render() {
     const { allExpenses, deleteExpenseProp, getExpenseToUpdateProp } = this.props;
     return (
-      <main>
-        <table>
-          <thead>
+      <main className="table-responsive">
+        <table className="table table-hover">
+          <thead className="thead-dark">
             <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
+              <th className="text-center">Descrição</th>
+              <th className="text-center">Tag</th>
+              <th className="text-center">Método de pagamento</th>
+              <th className="text-center">Valor</th>
+              <th className="text-center">Moeda</th>
+              <th className="text-center">Câmbio utilizado</th>
+              <th className="text-center">Valor convertido</th>
+              <th className="text-center">Moeda de conversão</th>
+              <th className="text-center">Editar/Excluir</th>
             </tr>
           </thead>
           <tbody>
@@ -27,36 +29,41 @@ class Table extends Component {
               id, description, tag, method,
               value, currency, exchangeRates,
             }) => {
-              const currencyName = exchangeRates[currency].name;
+              let currencyName = exchangeRates[currency].name;
+              if (currencyName.includes('/')) {
+                currencyName = currencyName.slice(0, currencyName.indexOf('/'));
+              }
               const exchangeUsed = Number(exchangeRates[currency].ask);
               const convertedValue = Number(exchangeRates[currency].ask) * value;
 
               return (
                 <tr key={ id }>
                   <td>{description || '-'}</td>
-                  <td>{tag}</td>
-                  <td>{method}</td>
-                  <td>{Number(value).toFixed(2)}</td>
-                  <td>{currencyName}</td>
-                  <td>{ exchangeUsed.toFixed(2) }</td>
-                  <td>{ convertedValue.toFixed(2) }</td>
-                  <td>Real</td>
-                  <td>
+                  <td className="text-center">{tag}</td>
+                  <td className="text-center">{method}</td>
+                  <td className="text-center">{Number(value).toFixed(2)}</td>
+                  <td className="text-center">{currencyName}</td>
+                  <td className="text-center">{ exchangeUsed.toFixed(2) }</td>
+                  <td className="text-center">{ convertedValue.toFixed(2) }</td>
+                  <td className="text-center">Real</td>
+                  <td className="text-center">
                     <button
                       type="button"
                       data-testid="edit-btn"
                       onClick={ () => getExpenseToUpdateProp(allExpenses
                         .find((expense) => expense.id === id)) }
+                      className="btn btn-outline-info btn-sm mr-1"
                     >
-                      Editar
+                      <img src={ editIcon } alt="Edit Icon Button" />
                     </button>
                     <button
                       type="button"
                       data-testid="delete-btn"
                       onClick={ () => deleteExpenseProp(allExpenses
                         .filter((expense) => expense.id !== id)) }
+                      className="btn btn-outline-danger btn-sm"
                     >
-                      Remover
+                      <img src={ deleteIcon } alt="Delete Icon Button" />
                     </button>
                   </td>
                 </tr>
