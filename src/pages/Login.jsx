67ece4b7111076b8cf520c9c6bbd06bbf9d3556d
Pedro Pereira <pedro.2108@hotmail.com>
2022-05-23 21:47:42 -0300

    style: login page styled

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login as loginAction } from '../actions';
+import ilustration from '../assets/ilustration-logo.svg';
 
 class Login extends React.Component {
   constructor() {
@@ -44,34 +45,50 @@ class Login extends React.Component {
   render() {
     const { email, password, isDisabled } = this.state;
     return (
-      <div>
-        <h2>Login Page</h2>
-        <section>
-          <input
-            type="email"
-            name="email"
-            placeholder="Insira seu e-mail"
-            value={ email }
-            onChange={ this.validateData }
-            data-testid="email-input"
+      <div
+        className="vh-100 d-flex flex-column align-items-center
+        justify-content-center bg-light"
+      >
+        <section
+          className="bg-white border border-light rounded p-5 shadow"
+        >
+          <img
+            src={ ilustration }
+            alt="Woman doing bitcoin trading Illustration by Lihin Souw on IconScout"
           />
-          <input
-            type="password"
-            name="password"
-            placeholder="Digita a senha"
-            value={ password }
-            onChange={ this.validateData }
-            data-testid="password-input"
-          />
-        </section>
-        <section>
-          <button
-            type="button"
-            disabled={ isDisabled }
-            onClick={ this.saveData }
+          <h2 className="d-flex justify-content-center">Trybewallet</h2>
+          <section
+            className="d-flex flex-column"
           >
-            Entrar
-          </button>
+            <input
+              type="email"
+              name="email"
+              placeholder="Insira seu e-mail"
+              value={ email }
+              onChange={ this.validateData }
+              data-testid="email-input"
+              className="mb-2 mt-4 form-control"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Digita a senha"
+              value={ password }
+              onChange={ this.validateData }
+              data-testid="password-input"
+              className="mb-4 form-control"
+            />
+          </section>
+          <section className="d-flex justify-content-center">
+            <button
+              type="button"
+              disabled={ isDisabled }
+              onClick={ this.saveData }
+              className="btn btn-primary w-75 py-2"
+            >
+              Entrar
+            </button>
+          </section>
         </section>
       </div>
     );
