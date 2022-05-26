1b7f96124831397d570ed3b65df411223ced08c3
Pedro Pereira <pedro.2108@hotmail.com>
2022-05-26 11:02:38 -0300

    feat: isLogged case implemented

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -3,12 +3,17 @@ import { LOGIN } from '../actions';
 
 const INITIAL_STATE = {
   email: '',
+  isLogged: false,
 };
 
 function userReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
   case LOGIN:
-    return action.email;
+    return {
+      ...state,
+      email: action.email,
+      isLogged: true,
+    };
   default:
     return state;
   }
